46d96fa4e6e4bceb3a2da6fca7215e943034d97e
Luis Ramalho <grandelois@hotmail.com>
2023-03-27 10:59:12 +0100

    Add project files.

---
+++ CityInfo.API/Program.cs
@@ -0,0 +1,69 @@
+using CityInfo.API.DbContexts;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //builder.Logging.ClearProviders();
+            //builder.Logging.AddConsole();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+            }
+                ).AddNewtonsoftJson()
+                .AddXmlDataContractSerializerFormatters();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
+            #if DEBUG
+            builder.Services.AddTransient<IMailService, LocalMailService>();
+            #else
+            builder.Services.AddTransient<IMailService, CloudMailService>();
+#endif
+
+            builder.Services.AddSingleton<CitiesDataStore>();
+            builder.Services.AddDbContext<CityInfoContext>(
+                dbContextOptions => dbContextOptions.UseSqlite(
+                    builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
+
+            builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            var app = builder.Build();
+
+            //// Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+            app.Run();
+        }
+    }
+}
